924302444ed583c1af5fc32c47edf62d4c7bef05
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-05-06 17:13:35 -0400

    First API App updates

---
+++ src/Components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Titles() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="display-4 mt-3">The Star Wars Library</h1>
+          <h2 className="h3 mt-3">A simple Star Wars API App</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Titles;
