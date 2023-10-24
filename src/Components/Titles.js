46020f1403688c4948525ce25a648b19727d34cf
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-10-24 06:55:39 -0400

    changed text

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -7,9 +7,7 @@ function Titles() {
         <div className="col text-center">
           <h1 className="display-4 mt-3">The Star Wars Library</h1>
           <h2 className="h3 mt-3">A Simple Star Wars API App</h2>
-          <h3 className="h5 mt-3">
-            Type the name of a Star Wars character and find out about them!
-          </h3>
+          <h3 className="h5 mt-3">Learn about all the Star Wars characters!</h3>
         </div>
       </div>
     </div>
